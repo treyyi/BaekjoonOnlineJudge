a3a11a2c5a6a2c671977bd7c1ee0f4ae3964c0e3
TreyYi <ctrkwang1@gmail.com>
2021-01-10 22:51:16 +0900

    sorting problem solution with Quick Sort (2751)

--- 2751.py
+++ 2751.py
@@ -1,9 +1,27 @@
-# Pypy3
-arr = []
+# Solution 1 (Pypy3)
+# arr = []
+#
+# for _ in range(int(input())):
+#     arr.append(int(input()))
+#
+# print(*sorted(arr), sep='\n')
 
+# Solution 2 (Quick sort solution)
+def quick_sort(arr):
+    #  End condition
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0] # Hoare Partition
+    tail = arr[1:]
+
+    left_arr = [x for x in tail if x <= pivot]
+    right_arr = [x for x in tail if x > pivot]
+
+    return quick_sort(left_arr) + [pivot] + quick_sort(right_arr)
+
+arr = []
 for _ in range(int(input())):
     arr.append(int(input()))
 
-print(*sorted(arr), sep='\n')
-
-# Quick sort solution will be added soon
+print(*quick_sort(arr), sep='\n')
